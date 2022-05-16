82b37e6458bb376e3bd4dc315f6b902b1f8c96ae
Jaseela99 <jaseelachittur@gmail.com>
2022-05-16 13:47:20 +0530

    2a

---
+++ 2.js
@@ -0,0 +1,5 @@
+//2.a
+//  if the length of string is less than 8 then "#" will be added to string until its length is 8 and it is consoled
+for (let string = "#"; string.length < 8; string += "#") {
+  console.log(string);
+}
