7a142d1b80c3290d7f70aa5025e42fa5419317aa
Jaseela99 <jaseelachittur@gmail.com>
2022-05-16 19:47:44 +0530

    completed 2

--- 2.js
+++ 2.js
@@ -13,9 +13,44 @@ const sevenRandomNumbers=()=>{
     for (let i=0;i<8;i++){
         //math.floor floors to the nearest integer
         //math.random generates a random number between 0 and 1
-        let num = Math.floor(Math.random()*10)
-        array.includes(num)?num:array.push(num)
+        const randoms=() => Math.floor(Math.random()*10)
+        let num =randoms()
+        console.log(num)
+        array.includes(num)?randoms():array.push(num)
+
     }
     return array
 }
 console.log(sevenRandomNumbers())
+
+//2.c
+let arrays=["A","B","C"]
+const reverseArray = (arrays) => {
+    //we only need to reverse half of the array if fully reversed it will be equal to old array
+    for (let i=0;i<(arrays.length/2);i++){
+        //storing "A" to temp
+        let temp=arrays[i]
+        //i=0 and arrays.length-1=2 storing "C" to 0th index
+        arrays[i]=arrays[arrays.length-1-i]
+        //storing "A" to last index
+        arrays[arrays.length-1-i]=temp
+        return arrays
+    }
+}
+console.log(reverseArray(arrays))
+
+//2.d
+const arrOne = [1, 4, 6, 2, 1];
+const arrTwo = [1, 4, 6, 2, 3]
+const checkUniqueness = (arr) => {
+       let unique = new Set(arr)
+       //if the length of unique is equal to the length of arr then it is unique and it returns true
+       return unique.size === arr.length ? true : false  
+}
+console.log(checkUniqueness(arrOne))
+console.log(checkUniqueness(arrTwo))
+
+
+
+
+
